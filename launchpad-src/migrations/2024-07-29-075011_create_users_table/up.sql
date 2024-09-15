17f4e4c2ff884886e1c40bc87db3252810310a4f
Ahmad Urabi <a.urabi@sagerdrone.com>
2024-09-15 15:47:50 +0300

    integration

--- launchpad-src/migrations/2024-07-29-075011_create_users_table/up.sql
+++
@@ -1,8 +0,0 @@
--- Your SQL goes here
-CREATE TABLE IF NOT EXISTS users (
-  id bigint unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  name VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL,
-  role VARCHAR(255) NOT NULL DEFAULT 'user',
-  password VARCHAR(255) NOT NULL
-)
--- launchpad-src/migrations/2024-07-29-075247_create_access_token_table/up.sql
+++
@@ -1,9 +0,0 @@
--- Your SQL goes here
-CREATE TABLE IF NOT EXISTS access_tokens (
-    id bigint unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    user_id bigint unsigned NOT NULL,
-    token VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    last_used TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(id)
-)
